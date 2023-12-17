86eed6004e3f9236aba9fb38acfccb85d52dda3b
yassinelakhdachi <lakhdachiyassine@gmail.com>
2023-12-17 18:03:04 +0100

    player se deplace vers Arena, Arena Graphics found with black vue

--- icmon/src/main/java/ch/epfl/cs107/icmon/area/Arena.java
+++ icmon/src/main/java/ch/epfl/cs107/icmon/area/Arena.java
@@ -3,16 +3,22 @@ package ch.epfl.cs107.icmon.area;
 import ch.epfl.cs107.play.engine.actor.Background;
 import ch.epfl.cs107.play.engine.actor.Foreground;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.RegionOfInterest;
 
 public final class Arena extends ICMonArea {
 
     @Override
     protected void createArea() {
-        registerActor(new Background(this));
-        registerActor(new Foreground(this));
+        // Chemin d'accès au fichier background
+        registerActor(new Background(this, "arena"));
+
+        // Chemin d'accès au fichier foreground et définition de la région d'intérêt
+//        RegionOfInterest roi = new RegionOfInterest(0, 0, 1000, 1000);
+//        registerActor(new Foreground(this, roi, "arena"));
+
+        // Ajout de la porte
         registerActor(new Door(this, "Town", new DiscreteCoordinates(20, 15),
                 new DiscreteCoordinates(4, 1)));
-
     }
 
     @Override
